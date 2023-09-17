781374a1530baff9641d98df14720dc712db91f4
Tuaannn <vantuan.97.pt@gmail.com>
2023-09-17 18:47:15 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+function change_color() {
+    var element = document.querySelectorAll("box");
+    element.addEventListener("click", function () {
+        alert("Bạn đã đúp chuột vào thẻ input này");
+    });
+}
